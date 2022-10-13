46d9a48fe5164f9cbec65f511791c5e301fae4ee
Thibaut Dubois <tdubois@1H2.42angouleme.fr>
2022-10-13 10:25:27 +0200

    first working test

--- test.bash
+++ test.bash
@@ -6,29 +6,25 @@
 #    By: tdubois <tdubois@marvin.42.fr>             +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/10/12 16:58:46 by tdubois           #+#    #+#              #
-#    Updated: 2022/10/12 17:51:42 by tdubois          ###   ########.fr        #
+#    Updated: 2022/10/13 10:24:14 by tdubois          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 run="$(pwd)/test.out"
 
-read -r -d '' input <<'EOF'
-jawdijawid
-wdajwdiawd
-awdaw
-EOF
+test__read_one_line_from_stdin() {
+	read -r -d '' input <<-EOF
+		jawdijawid
+		wdajwdiawd
+		awdaw
+	EOF
 
+	read -r -d '' expected_output <<-EOF
+		jawdijawid
+	EOF
 
-echo $input
-
-output=$(cat << 'EOF'
-jawdijawid
-EOF
-)
-
-
-testEquality() {
-	assertEquals "$(echo $input | $run 1 0)" "$output"
+	assertEquals "$expected_output" "$(echo "$input" | $run 1 0)"
+	return 0
 }
 
 source shunit2/shunit2
