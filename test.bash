d56605a2371f4c95fcd5d39162b29c42ead19a1f
Thibaut Dubois <tdubois@1H2.42angouleme.fr>
2022-10-13 11:32:05 +0200

    run tests through valgrind
    echoes valgrind output only on error

--- test.bash
+++ test.bash
@@ -6,11 +6,21 @@
 #    By: tdubois <tdubois@marvin.42.fr>             +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/10/12 16:58:46 by tdubois           #+#    #+#              #
-#    Updated: 2022/10/13 10:24:14 by tdubois          ###   ########.fr        #
+#    Updated: 2022/10/13 11:29:32 by tdubois          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-run="$(pwd)/test.out"
+bin="$(pwd)/test.out"
+
+function run() {
+	local output
+	output="$(cat - | valgrind --error-exitcode=1 $bin $1 $2 2>&1)"
+	if [ $? -eq 1 ] #if valgrind threw error
+	then
+		echo -e "\n--> Valgrind error\n$output\n" 1>&2
+	fi
+	echo "$output" | grep -v ==
+}
 
 test__read_one_line_from_stdin() {
 	read -r -d '' input <<-EOF
@@ -22,8 +32,10 @@ test__read_one_line_from_stdin() {
 	read -r -d '' expected_output <<-EOF
 		jawdijawid
 	EOF
+	
+	output="$(echo "$input" | run 1 0)"
 
-	assertEquals "$expected_output" "$(echo "$input" | $run 1 0)"
+	assertEquals "$expected_output"	"$output"
 	return 0
 }
 
